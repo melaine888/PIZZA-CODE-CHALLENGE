e4ffa468ce4cc6b50a3cd76bf3c43ea33a1eeea4
Melaine Kanana <melaine.kanana@student.moringaschool.com>
2023-07-17 08:18:49 +0300

    commit 8

--- app/app.py
+++ app/app.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python3
+from flask import Flask, request, jsonify, abort
+from flask_migrate import Migrate
+from models import db, Restaurant, Pizza, RestaurantPizza
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+migrate = Migrate(app, db)
+
+
+if __name__ == '__main__':
+    app.run(port=5501, debug=True)
