db2f83e68337b1c10047720930769f80891ac170
Melaine Kanana <melaine.kanana@student.moringaschool.com>
2023-07-19 08:14:18 +0300

    commit 9

--- app/app.py
+++ app/app.py
@@ -10,6 +10,25 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 migrate = Migrate(app, db)
 
+@app.route('/Home')
+def home():
+    return '<h2>Flask app for Restaurants</h2>'
+
+@app.route('/restaurants')
+def get_restaurants():
+    restaurants = Restaurant.query.all()
+    restaurant_list = []
+    for restaurant in restaurants:
+        restaurant_info = {
+            'id': restaurant.id,
+            'name': restaurant.name,
+            'address': restaurant.address
+        }
+        restaurant_list.append(restaurant_info)
+    return jsonify(restaurant_list)
+
+
+
 
 if __name__ == '__main__':
     app.run(port=5501, debug=True)
